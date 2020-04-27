7ec6dd288bde7a4cebf2ebec0d555511bf6f1b67
Tolu Adesegha <tolu@adesegha.ca>
2020-04-26 18:59:02 -0600

    plot data and compute cost for exercise 1

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+hx = X * theta;
+error = hx - y;
+squaredError = error .^ 2;
+J = sum(squaredError) / 2 / m;
 
 % =========================================================================
 
